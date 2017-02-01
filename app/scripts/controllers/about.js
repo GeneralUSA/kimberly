272db88ca01eff97796e52f2b7a273d8ccf1bb85
Chris Phyffer <cphyffer.123@gmail.com>
2017-02-01 12:35:39 -0800

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kimberlyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kimberlyApp
+ */
+angular.module('kimberlyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kimberlyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kimberlyApp
+ */
+angular.module('kimberlyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
